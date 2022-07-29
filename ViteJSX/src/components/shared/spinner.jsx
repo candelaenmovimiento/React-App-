aafc71f24ccaf1d3632b394c43ff092992c3d0e4
candelaenmovimiento <102130501+candelaenmovimiento@users.noreply.github.com>
2022-07-29 17:42:27 -0300

    update sipnners size

--- ViteJSX/src/components/shared/spinner.jsx
+++ ViteJSX/src/components/shared/spinner.jsx
@@ -5,7 +5,7 @@ function Spinner() {
     <img
       src={spinner}
       alt="Loading..."
-      style={{ width: 50, margin: "auto", display: "block" }}
+      style={{ width: "13%", margin: "auto", display: "block" }}
     />
   );
 }
