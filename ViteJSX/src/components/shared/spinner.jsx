91849287f2df3e3338cdb0c891388b85b143db1e
candelaenmovimiento <102130501+candelaenmovimiento@users.noreply.github.com>
2022-07-29 16:46:34 -0300

    spinner mas pequeño mas estetico

--- ViteJSX/src/components/shared/spinner.jsx
+++ ViteJSX/src/components/shared/spinner.jsx
@@ -5,7 +5,7 @@ function Spinner() {
     <img
       src={spinner}
       alt="Loading..."
-      style={{ width: 1000, margin: "auto", display: "block" }}
+      style={{ width: 50, margin: "auto", display: "block" }}
     />
   );
 }
